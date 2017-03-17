2f56d2c7ce850a3e6a61470215cc4471fce891ca
awegrzyn <adam.wegrzynek@cern.ch>
2017-03-17 09:56:42 +0100

    eslintrc init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "root": true,
+  "extends": ["eslint:recommended", "google"],
+  "env": {
+    "node": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6
+  }, 
+  "rules": {
+  }
+};
