68c7ebedd2197905b04b335d12d5ae026ff2a40a
awegrzyn <adam.wegrzynek@cern.ch>
2017-03-21 15:02:02 +0100

    eslint comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
   "rules": {
     "max-len": ['error', {code: 100}], // default 80
     "indent": ['error', 2, {"SwitchCase": 1}], // not set by default
-    "comma-dangle": ["error", "never"], // do not enforce trailing comma (?)
-    "one-var": [2, { // forces having single declaration in the begining of file (const) ?? - it's not really consistat
+    "comma-dangle": ["error", "never"], // do not enforce trailing comma
+    "one-var": [2, { // forces having single declaration keyword
       var: 'always',
       let: 'always',
       const: 'always',
