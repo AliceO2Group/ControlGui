54a0d26262fe5a6f9f479660fcb75b7925d1cb7b
awegrzyn <adam.wegrzynek@cern.ch>
2017-03-28 15:52:52 +0200

    configurable log level

--- log.js
+++ log.js
@@ -1,12 +1,13 @@
 const winston = require('winston');
+const config = require('./config.json');
 
 module.exports = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)(
-      {timestamp: true, colorize: true, level: 'debug'}
+      {timestamp: true, colorize: true, level: config.log.console}
     ),
     new winston.transports.File(
-     {filename: './error.log', level: 'error'}
+     {filename: './error.log', level: config.log.file}
     )
   ],
   exitOnError: true
