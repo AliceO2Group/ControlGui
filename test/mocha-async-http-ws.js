481f33a77a014ab7b39933a91c878269a9851b41
awegrzyn <adam.wegrzynek@cern.ch>
2017-03-28 15:57:07 +0200

    eslint corrections

--- test/mocha-async-http-ws.js
+++ test/mocha-async-http-ws.js
@@ -19,7 +19,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 chai.use(chaiHttp);
 
 const http = new HttpServer(credentials, app);
-const websocketServer = new WebSocket(http.server);
+new WebSocket(http.server);
 const jwt = new JwtToken(config.jwt);
 const token = jwt.generateToken(0, 'test', 1);
 
@@ -28,8 +28,8 @@ describe('rest-api', () => {
     chai.request(http.httpsServer).get('/api/runs').end((err, res) => {
       assert.equal(res.status, 403, 'Wrong HTTP response code');
       done();
-    }); 
-  }); 
+    });
+  });
 
   it('should get response', (done) => {
     chai.request(http.httpsServer)
@@ -38,15 +38,19 @@ describe('rest-api', () => {
       .end((err, res) => {
         assert.equal(res.status, 200, 'Wrong HTTP response code');
         done();
-      }); 
-  }); 
+      });
+  });
 });
 
 describe('websocket', () => {
   it('should successfully connect to websocket server', (done) => {
-    const connection = new WebSocketClient('wss://localhost:' + config.http.portSecure +'/?token=' + token);
+    const connection = new WebSocketClient(
+     'wss://localhost:' + config.http.portSecure +'/?token=' + token
+    );
     connection.on('open', () => {
-      setTimeout(() => { done(); }, 250);
+      setTimeout(() => {
+        done();
+      }, 250);
     });
     connection.on('close', () => {
       throw new Error('disconnected from ws server');
