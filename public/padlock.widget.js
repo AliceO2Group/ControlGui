9ec9b2cbba954493a332ca9803660a4935bbcf12
Adam Wegrzynek <adam.wegrzynek@cern.ch>
2017-08-15 13:17:01 +0200

    Improved widget structure: padlock

--- public/padlock.widget.js
+++ public/padlock.widget.js
@@ -1,23 +1,40 @@
+/**
+ * Padlock jQuery UI widget. Displays whether the interface is locked.
+ * Required options:
+ *  - (none)
+ * Internal options
+ *  - locked - flag that states whether it's locked or not
+ *  - id - person ID that holds the lock
+ * @author Adam Wegrzynek <adam.wegrzynek@cern.ch>
+ */
+
 $.widget('o2.padlock', {
-  options: {
-    locked: undefined,
-    id: undefined
-  },
+  /**
+   * Clear all classes from the element, sets back default color
+   */ 
   _clear: function() {
-    this.element.removeClass('ui-icon-locked').removeClass('ui-icon-unlocked');
+    this._removeClass('ui-icon-locked')._removeClass('ui-icon-unlocked');
     this.element.css('color', 'black');
   },
+
+  /**
+   * Sets icon to locked, stored the id
+   */ 
   lock: function(id) {
     this._clear();
     if (this.options.id != id) {
       this.element.css('color', 'red');
     }
     this.options.locked = true;
-    this.element.addClass('ui-icon-locked');
+    this._addClass('ui-icon-locked');
   },
+
+  /**
+   * Sets icon to unlocked
+   */
   unlock: function() {
     this.options.locked = false;
     this._clear();
-    this.element.addClass('ui-icon-unlocked');
+    this._addClass('ui-icon-unlocked');
   }
 });
